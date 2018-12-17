c99fd1fe23d2295a52bd754ae0a4d736d4948018
Kruddyy <43756292+Kruddyy@users.noreply.github.com>
2018-12-17 12:48:38 -0500

    Add files via upload

---
+++ lesson 9/problem 2/problem1.py
@@ -0,0 +1,12 @@
+print('-'*60)
+print('Locker Bot:')
+print()
+password = input('Whats the password? ')
+if password == 'Rose Bud':
+		print('Password invalid ... enabling laser defense system, please stand down!!!')
+		print('Pew Pew Pew!')
+else:
+		print('Password invalid ... enabling laser defense system, please stand down!!!')
+		print('Pew Pew Pew!')
+
+print('-'*60)
---
+++ lesson 9/problem 3/problem3.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Welcome to CLUB425, the most lit club in downtown ACTvf. Before ou can enter, I need ou to answer some questions ...')
+print()
+password = input('What is your age today? ')
+if password == '24':
+		print('Cool come on in.')
+else:
+		print('Your gonna need to back up. THis club is 21+ onl so find somewhere else to part or findout what robot punch feels like'
+
+print('-'*60)
